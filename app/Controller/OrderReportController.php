5db4f5a0b78a9faf0cb89001f834f99ff5d4661f
Godwinh Lopez <godwinh@photoup.net>
2020-07-05 16:47:59 +0800

    answer to question #5

--- app/Controller/OrderReportController.php
+++ app/Controller/OrderReportController.php
@@ -12,8 +12,8 @@
 			$this->loadModel('Portion');
 			$portions = $this->Portion->find('all',array('conditions'=>array('Portion.valid'=>1),'recursive'=>2));
 			// debug($portions);exit;
-
-
+            
+            /*
 			// To Do - write your own array in this format
 			$order_reports = array('Order 1' => array(
 										'Ingredient A' => 1,
@@ -32,6 +32,64 @@
 								  		'Ingredient D' => 6,
 								  	),
 								);
+                                
+                                */
+                               
+            $itemPartsMapping = array();
+            
+            /*
+            the mapping looks similar to this: 
+            $itemPartsMapping = array(
+                <itemId> => array(
+                     <partId> => array( 
+                        'name' => 'Ingredient A', 
+                        'value' => 6
+                    ), 
+                )
+            );
+            */
+            
+            // creating map
+            foreach ($portions as $portion) {
+                $itemId = $portion['Item']['id'];
+                
+                if (!isset($itemPartsMapping[$itemId])) {
+                     $itemPartsMapping[$itemId] = array();
+                }
+                
+                if (isset($portion['PortionDetail'])) {
+                     foreach ($portion['PortionDetail'] as $detail) {
+                         $itemPartsMapping[$itemId][$detail['part_id']] = array(
+                             'name' => $detail['Part']['name'], 
+                             'value' => (int) $detail['value']
+                         );
+                     }
+                }
+            }
+             
+             $order_reports = []; 
+             
+             foreach ($orders as $order) {
+                 $orderKey = $order['Order']['name'];
+                 
+                 if (!isset($order_reports[$orderKey])) {
+                     $order_reports[$order['Order']['name']] = array();
+                 }
+                 
+                 foreach ($order['OrderDetail'] as $orderDetail) {
+                     $item = $orderDetail['Item']; 
+                     
+                     foreach ($itemPartsMapping[$item['id']] as $part) {
+                         if (!isset($order_reports[$orderKey][$part['name']])) {
+                             $order_reports[$orderKey][$part['name']] = 0;
+                         }
+                         
+                         $order_reports[$orderKey][$part['name']] += $part['value'];
+                     }
+                 }
+                 
+                 ksort($order_reports[$orderKey]);
+             }
 
 			// ...
 
